b84fbfd79b492301d0277445db81d4d1dd088864
Kirill <kirilltcoi@yandex.ru>
2022-08-03 18:20:35 +0700

    init

--- App.tsx
+++ App.tsx
@@ -1,20 +1,39 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import Main from './components/Main';
+
+import * as Font from 'expo-font';
+import { gStyle } from './styles/style';
+
+const fonts = () =>
+    Font.loadAsync({
+        'Montserrat-Bold': require('./assets/fonts/Montserrat-Bold.ttf'),
+        'Montserrat-Light': require('./assets/fonts/Montserrat-Light.ttf'),
+    });
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [fontLoaded, setFontLoaded] = useState(false);
+
+    if (!fontLoaded) {
+        return (
+            <AppLoading
+                startAsync={fonts}
+                onFinish={() => setFontLoaded(true)}
+                onError={console.warn}
+            />
+        );
+    }
+
+    return <Main />;
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
